f1d08458cb3f17df68238067352833594a999a9f
Anjany Pandey <anjany.pandey06@gmail.com>
2025-01-04 15:36:06 +0530

    Backedn and database setup

---
+++ backend/db/connectDB.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+export const connectDB = async() =>{
+  try {
+    console.log(process.env.MONGODB_URI)
+    const conn = await mongoose.connect(`${process.env.MONGODB_URI}`)
+    console.log(`MONGODB Connected : ${conn.connection.host}`)
+  } catch (error) {
+    console.log(error.message)
+    process.exit(1)//1 is failure, status code is success
+  }
+
+}
