bdc425851fec4d292ba7bdfc00b1c32c41a9f300
Anjany Pandey <anjany.pandey06@gmail.com>
2025-01-04 19:44:05 +0530

    login route done

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,5 +1,5 @@
 import userModel from "../models/userModel.js";
-import bcryptjs from "bcryptjs";
+import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 
 export const register = async (req, res) => {
@@ -16,7 +16,7 @@ export const register = async (req, res) => {
         .json({ success: false, message: "User Already exits" });
     }
 
-    const hashedPassword = await bcryptjs.hash(password, 10);
+    const hashedPassword = await bcrypt.hash(password, 10);
 
     const user = new userModel({
       email,
@@ -40,12 +40,43 @@ export const register = async (req, res) => {
 
     res.json({ success: true, message: "New user created" });
   } catch (error) {
-    res.status(400).json({ success: false, message: error.message });
+    return res.status(400).json({ success: false, message: error.message });
   }
 };
 
 export const login = async (req, res) => {
-  res.send("login Route");
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.json({ success: false, message: "All fields required" });
+  }
+
+  try {
+    const user = await userModel.findOne({ email });
+    if (!user) {
+      return res.json({ success: false, message: "Invalid email" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.json({ success: false, message: "Invalid password" });
+    }
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+
+    res.cookie("token", token, {
+      httpOnly: true, //cookie cannot be accessed by client side js
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict", //it prevents csrf attack
+      maxAge: 7 * 24 * 60 * 60 * 1000, // valid for 7 days
+    });
+
+    return res.json({ success: true, message: "user logged in" });
+  } catch (error) {
+    return res.status(400).json({ success: false, message: error.message });
+  }
 };
 
 export const logout = async (req, res) => {
