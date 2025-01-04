c4e0ded5642b098a1b50c58289360a73c85f73c4
Anjany Pandey <anjany.pandey06@gmail.com>
2025-01-04 15:37:22 +0530

    fix

--- backend/db/connectDB.js
+++ backend/db/connectDB.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const connectDB = async() =>{
   try {
     console.log(process.env.MONGODB_URI)
-    const conn = await mongoose.connect(`${process.env.MONGODB_URI}`)
+    const conn = await mongoose.connect(process.env.MONGODB_URI)
     console.log(`MONGODB Connected : ${conn.connection.host}`)
   } catch (error) {
     console.log(error.message)
