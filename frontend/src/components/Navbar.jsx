474c8ecd159b7e5c00c4638b42d33f9b98610c82
Anjany Pandey <anjany.pandey06@gmail.com>
2025-01-06 03:18:21 +0530

    menu added

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,15 +3,28 @@ import { assets } from "../assets/assets";
 import { useNavigate } from "react-router-dom";
 import { useContext } from "react";
 import { AppContext } from "../Context/AppContext";
+import { MdMenu } from "react-icons/md";
 const Navbar = () => {
   const navigate = useNavigate();
-  const { userData, backendUrl, setUserData, setIsLoggedin } =
+  const { userData, backendUrl, setUserData, isLoggedin, setIsLoggedin } =
     useContext(AppContext);
   return (
     <div className="w-full flex justify-between items-center p-4 sm:p-6 sm:px-24 absolute top-0">
       <img src={assets.logo} alt="" className="w-28 sm:w-32" />
-      {userData ? (
-        <div>{userData.name[0].toUpperCase()}</div>
+      {!isLoggedin ? (
+        <div className="w-8 h-8 flex justify-center items-center rounded-full bg-black text-white relative group">
+          <MdMenu />
+          <div className="absolute hidden group-hover:block top-0 right-0 z-10 text-black rounded pt-10">
+            <ul className="list-none m-0 p-2 bg-gray-100 text-sm">
+              <li className="py-1 px-2 hover:bg-gray-200 cursor-pointer">
+                Verify Email
+              </li>
+              <li className="py-1 px-2 hover:bg-gray-200 cursor-pointer pr-10">
+                Logout
+              </li>
+            </ul>
+          </div>
+        </div>
       ) : (
         <button
           onClick={() => navigate("/login")}
