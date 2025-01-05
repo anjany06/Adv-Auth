2e6aca60d4a2ac51d90b81146742b7d14b9332ea
Anjany Pandey <anjany.pandey06@gmail.com>
2025-01-06 03:21:40 +0530

    fix

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -12,6 +12,14 @@ const Navbar = () => {
     <div className="w-full flex justify-between items-center p-4 sm:p-6 sm:px-24 absolute top-0">
       <img src={assets.logo} alt="" className="w-28 sm:w-32" />
       {!isLoggedin ? (
+        <button
+          onClick={() => navigate("/login")}
+          className="flex items-center gap-2 border border-gray-500 rounded-full px-6 py-2 text-gray-800 hover:bg-gray-100 transition-all"
+        >
+          Login
+          <img src={assets.arrow_icon} alt="" />
+        </button>
+      ) : (
         <div className="w-8 h-8 flex justify-center items-center rounded-full bg-black text-white relative group">
           <MdMenu />
           <div className="absolute hidden group-hover:block top-0 right-0 z-10 text-black rounded pt-10">
@@ -25,14 +33,6 @@ const Navbar = () => {
             </ul>
           </div>
         </div>
-      ) : (
-        <button
-          onClick={() => navigate("/login")}
-          className="flex items-center gap-2 border border-gray-500 rounded-full px-6 py-2 text-gray-800 hover:bg-gray-100 transition-all"
-        >
-          Login
-          <img src={assets.arrow_icon} alt="" />
-        </button>
       )}
     </div>
   );
