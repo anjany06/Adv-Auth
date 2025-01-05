97f7545fac65cdc179f73c7487c14e8528b7599b
Anjany Pandey <anjany.pandey06@gmail.com>
2025-01-05 17:43:54 +0530

    Routes setUp

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import EmailVerify from "./pages/EmailVerify";
+import ResetPassword from "./pages/ResetPassword";
 
 const App = () => {
-  return ( <div></div>);
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/email-verify" element={<EmailVerify />}></Route>
+        <Route path="/reset-password" element={<ResetPassword />}></Route>
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
