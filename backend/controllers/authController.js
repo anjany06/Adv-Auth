599f67c4e781c23b35b2812508c8f802fb786452
Anjany Pandey <anjany.pandey06@gmail.com>
2025-01-06 21:56:38 +0530

    Feat: email template added and project done

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -2,6 +2,10 @@ import userModel from "../models/userModel.js";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import transporter from "../utils/nodemailer.js";
+import {
+  EMAIL_VERIFY_TEMPLATE,
+  PASSWORD_RESET_TEMPLATE,
+} from "../config/emailTemplates.js";
 
 export const register = async (req, res) => {
   const { email, password, name } = req.body;
@@ -128,7 +132,11 @@ export const sendVerifyOtp = async (req, res) => {
       from: process.env.SENDER_EMAIL,
       to: user.email,
       subject: "Account Verification OTP",
-      text: `Your OTP is ${otp}. Verify your account using this OTP.`,
+      // text: `Your OTP is ${otp}. Verify your account using this OTP.`,
+      html: EMAIL_VERIFY_TEMPLATE.replace("{{otp}}", otp).replace(
+        "{{email}}",
+        user.email
+      ),
     };
 
     await transporter.sendMail(mailOptions);
@@ -208,6 +216,10 @@ export const sendResetOtp = async (req, res) => {
       to: user.email,
       subject: "Password Reset OTP",
       text: `Your OTP for reset password is ${otp}. Use this OTP to proceed with resetting your password.`,
+      html: PASSWORD_RESET_TEMPLATE.replace("{{otp}}", otp).replace(
+        "{{email}}",
+        user.email
+      ),
     };
 
     await transporter.sendMail(mailOptions);
