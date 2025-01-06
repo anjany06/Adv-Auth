c7c5711df4eceeea890774807c78d9463a047fdd
Anjany Pandey <anjany.pandey06@gmail.com>
2025-01-07 01:35:19 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
+app.use(cors({ origin: "https://adv-auth-ctmc.vercel.app" ,credentials: true }));
 app.use(cookieParser());
 app.use(express.json()); //allows us to parse incoming requests: req.body
 
