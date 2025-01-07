6a66873c93999174847ec2220248c0805ce802ac
Anjany Pandey <anjany.pandey06@gmail.com>
2025-01-07 16:23:25 +0530

    cors

--- backend/server.js
+++ backend/server.js
@@ -10,9 +10,10 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-//using this we can access backend from any ip
 
-app.use(cors());
+app.use(
+  cors({ origin: "https://adv-auth-iota.vercel.app", credentials: true })
+);
 app.use(cookieParser());
 app.use(express.json()); //allows us to parse incoming requests: req.body
 
